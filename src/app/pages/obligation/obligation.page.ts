498b95da2e6b8a01e804086b757beba53787c358
Svathna <s.s.vathna@gmail.com>
2020-07-06 14:06:54 +0700

    hehe table

--- src/app/pages/obligation/obligation.page.ts
+++ src/app/pages/obligation/obligation.page.ts
@@ -2,12 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { Router } from '@angular/router';
 
+export const MONTHS = [
+  'មករា',
+  'កុម្ភៈ',
+  'មីនា',
+  'មេសា',
+  'ឧសភា',
+  'មិថុនា',
+  'កក្កដា',
+  'សីហា',
+  'កញ្ញា',
+  'តុលា',
+  'វិច្ឆិកា',
+  'ធ្នូ',
+];
+
 @Component({
   selector: 'app-obligation',
   templateUrl: './obligation.page.html',
   styleUrls: ['./obligation.page.scss'],
 })
 export class ObligationPage implements OnInit {
+  arrayMonths = MONTHS;
+  currentDate = new Date();
 
   constructor(
     private navCtr: NavController,
