b9182942c3bd2d52f34b0f3f1b279be24b0592d7
Svathna <s.s.vathna@gmail.com>
2020-07-26 04:43:32 +0700

    change status bar color XD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,7 @@ export class AppComponent {
     private authService: AuthService,
     private companyService: CompanyService,
   ) {
+    //*** Control SplashScreen
     this.splashScreen.show();
     this.initializeApp();
     this.fetData();
@@ -26,7 +27,10 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
+      //*** Control Status Bar
       this.statusBar.styleDefault();
+      this.statusBar.backgroundColorByHexString('#3069bb');
+      //*** Control SplashScreen
       this.splashScreen.hide();
     });
   }
