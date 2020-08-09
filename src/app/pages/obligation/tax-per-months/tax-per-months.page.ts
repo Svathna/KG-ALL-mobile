95569574540e8ec86f83f123e95e401be736dec3
Svathna <s.s.vathna@gmail.com>
2020-08-10 05:25:32 +0700

    On company status page

--- src/app/pages/obligation/tax-per-months/tax-per-months.page.ts
+++ src/app/pages/obligation/tax-per-months/tax-per-months.page.ts
@@ -1,57 +1,78 @@
-import { Component, OnInit } from '@angular/core';
-import { TaxPerMonth, TaxHistoryResponse } from 'src/app/interfaces/tax-per-month.interface';
-import { MONTHS } from '../obligation.page';
-import { TaxHistory } from 'src/app/models/company.model';
-import { NavController } from '@ionic/angular';
-import { CompanyService } from 'src/app/services/company.service';
-import { TaxPerHistoryService } from 'src/app/services/tax-history.service';
+import { Component, OnInit } from "@angular/core";
+import {
+    TaxPerMonth,
+    TaxHistoryResponse,
+} from "src/app/interfaces/tax-per-month.interface";
+import { TaxHistory } from "src/app/models/company.model";
+import { NavController } from "@ionic/angular";
+import { CompanyService } from "src/app/services/company.service";
+import { TaxPerHistoryService } from "src/app/services/tax-history.service";
+
+const MONTHS = [
+    'JAN',
+    'FEB',
+    'MARCH',
+    'APRIL',
+    'MAY',
+    'JUNE',
+    'JULY',
+    'AUGUST',
+    'SEPTEMBER',
+    'OCTOBER',
+    'NOVEMBER',
+    'DECEMBER',
+];
 
 @Component({
-  selector: 'app-tax-per-months',
-  templateUrl: './tax-per-months.page.html',
-  styleUrls: ['./tax-per-months.page.scss'],
+    selector: "app-tax-per-months",
+    templateUrl: "./tax-per-months.page.html",
+    styleUrls: ["./tax-per-months.page.scss"],
 })
 export class TaxPerMonthsPage implements OnInit {
-  arrayMonths = MONTHS;
-  currentDate = new Date();
-  taxHistory: TaxHistory;
-  taxPerMonths: TaxPerMonth[] = [];
-  isFetching = false;
+    arrayMonths = MONTHS;
+    currentDate = new Date();
+    taxHistory: TaxHistory;
+    taxPerMonths: TaxPerMonth[] = [];
+    isFetching = false;
 
-  constructor(
-    private navCtr: NavController,
-    private companyService: CompanyService,
-    private taxHistoryService: TaxPerHistoryService,
-  ) { }
+    constructor(
+        private navCtr: NavController,
+        private companyService: CompanyService,
+        private taxHistoryService: TaxPerHistoryService
+    ) {}
 
-  ngOnInit() {
-    this.fetchTaxhistory();
-  }
-
-  backHome() {
-    this.navCtr.navigateBack("home");
-  }
-
-  fetchTaxhistory() {
-    this.isFetching = true;
-    this.companyService.getTaxHistory().subscribe((data: TaxHistoryResponse) => {
-      this.isFetching = false;
-      if (data && data.taxHistory) {
-        this.taxHistory = data.taxHistory;
-        this.buildTaxPerMonths();
-      }
-    });
-  }
-
-  async buildTaxPerMonths(){
-    let taxPerMonths: TaxPerMonth[];
-    if (!this.taxHistory.taxPerMonths) {
-      taxPerMonths = [];
+    ngOnInit() {
+        this.fetchTaxhistory();
     }
-    this.isFetching = true;
-    const arrayData: TaxPerMonth[] = await this.taxHistoryService.buildTaxPerMonths(taxPerMonths ? taxPerMonths : this.taxHistory.taxPerMonths);
-    this.taxPerMonths = [...arrayData];
-    this.isFetching = false;
-    console.log(this.taxPerMonths);
-  }
+
+    backHome() {
+        this.navCtr.navigateBack("home");
+    }
+
+    fetchTaxhistory() {
+        this.isFetching = true;
+        this.companyService
+            .getTaxHistory()
+            .subscribe((data: TaxHistoryResponse) => {
+                this.isFetching = false;
+                if (data && data.taxHistory) {
+                    this.taxHistory = data.taxHistory;
+                    this.buildTaxPerMonths();
+                }
+            });
+    }
+
+    async buildTaxPerMonths() {
+        let taxPerMonths: TaxPerMonth[];
+        if (!this.taxHistory.taxPerMonths) {
+            taxPerMonths = [];
+        }
+        this.isFetching = true;
+        const arrayData: TaxPerMonth[] = await this.taxHistoryService.buildTaxPerMonths(
+            taxPerMonths ? taxPerMonths : this.taxHistory.taxPerMonths
+        );
+        this.taxPerMonths = [...arrayData];
+        this.isFetching = false;
+        console.log(this.taxPerMonths);
+    }
 }
