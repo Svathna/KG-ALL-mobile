e5c0261d40b58ab82d0c66f68df9a4defa2f9f08
Svathna <s.s.vathna@gmail.com>
2020-08-10 04:23:21 +0700

    implement search

--- src/app/pages/home/document-inventory/document-inventory.module.ts
+++ src/app/pages/home/document-inventory/document-inventory.module.ts
@@ -7,11 +7,13 @@ import { IonicModule } from '@ionic/angular';
 import { DocumentInventoryPageRoutingModule } from './document-inventory-routing.module';
 
 import { DocumentInventoryPage } from './document-inventory.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    TranslateModule.forChild(),
     ReactiveFormsModule,
     IonicModule,
     DocumentInventoryPageRoutingModule
--- src/app/pages/home/document-inventory/document-inventory.page.ts
+++ src/app/pages/home/document-inventory/document-inventory.page.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from "@angular/core";
-import { NavController, ToastController } from '@ionic/angular';
-import { CompanyService } from 'src/app/services/company.service';
-import { Doc, DocResponse } from 'src/app/models/company.model';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
-import { HelpersService } from 'src/app/services/helpers.service';
+import { NavController, ToastController } from "@ionic/angular";
+import { CompanyService } from "src/app/services/company.service";
+import { Doc, DocResponse, OtherDocument } from "src/app/models/company.model";
+import {
+    FormGroup,
+    FormBuilder,
+    Validators,
+    FormControl,
+} from "@angular/forms";
+import { HelpersService } from "src/app/services/helpers.service";
+import { debounce, debounceTime } from 'rxjs/operators';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: "app-document-inventory",
@@ -12,72 +19,108 @@ import { HelpersService } from 'src/app/services/helpers.service';
 })
 export class DocumentInventoryPage implements OnInit {
 	docs: Doc;
-	isFetching = false;
-	requestForm: FormGroup;
+	otherDocsFiltered: OtherDocument[] = [];
+    isFetching = false;
+    requestForm: FormGroup;
 	companyId: string;
+	currentLang = 'kh';
 
     constructor(
-		private navCtr: NavController,
-		private companyService: CompanyService,
-		private fb: FormBuilder,
-		private toast: ToastController,
-        private helpersService: HelpersService,
-	) {}
+        private navCtr: NavController,
+        private companyService: CompanyService,
+        private fb: FormBuilder,
+        private toast: ToastController,
+		private helpersService: HelpersService,
+		private translate: TranslateService,
+    ) {}
 
-	ngOnInit() {
-		this.fetchDocData();
+    ngOnInit() {
+        this.fetchDocData();
 		this.builRequestForm();
-	}
+		this.currentLang = this.translate.currentLang;
+    }
 
-	builRequestForm() {
-		this.requestForm = this.fb.group({
-			description: new FormControl('', [Validators.required, Validators.minLength(5)]),
-		});
-	}
+    builRequestForm() {
+        this.requestForm = this.fb.group({
+            description: new FormControl("", [
+                Validators.required,
+                Validators.minLength(5),
+            ]),
+        });
+    }
 
-	async submitRequest() {
-		if (this.requestForm.invalid) {
-			return;
-		}
-		this.isFetching = true;
-		const companyId = await this.companyService.getCompanyId();
-		const value = this.requestForm.value;
-		this.requestForm.reset();
-		Object.assign(value, { companyId });
-		this.companyService.sendRequest(value).subscribe((data: any) => {
-			this.isFetching = false;
-			if (data && data.success) {
-				this.toasterRequestSuccess();
-			}
-		})
-	}
+    async submitRequest() {
+        if (this.requestForm.invalid) {
+            return;
+        }
+        this.isFetching = true;
+        const companyId = await this.companyService.getCompanyId();
+        const value = this.requestForm.value;
+        this.requestForm.reset();
+        Object.assign(value, { companyId });
+        this.companyService.sendRequest(value).subscribe((data: any) => {
+            this.isFetching = false;
+            if (data && data.success) {
+                this.toasterRequestSuccess();
+            }
+        });
+    }
 
-	toasterRequestSuccess() {
-		const toast = this.toast.create({
-			message: 'ការស្នើត្រូវបានបញ្ជូនទៅក្នុងប្រព័ន្ធ',
-			duration: 3000,
-			position: 'bottom'
-		}).then(data => {
-			data.present();
-		});
-	}
-	
-	fetchDocData() {
-		this.isFetching = true;
-		this.companyService.getCompanyDocs().subscribe((data: DocResponse) => {
-			this.isFetching = false;
-			if (data && data.doc) {
+    toasterRequestSuccess() {
+        const toast = this.toast
+            .create({
+                message: "ការស្នើត្រូវបានបញ្ជូនទៅក្នុងប្រព័ន្ធ",
+                duration: 3000,
+                position: "bottom",
+            })
+            .then((data) => {
+                data.present();
+            });
+    }
+
+    fetchDocData() {
+        this.isFetching = true;
+        this.companyService.getCompanyDocs().subscribe((data: DocResponse) => {
+            this.isFetching = false;
+            if (data && data.doc) {
 				this.docs = data.doc;
+				this.otherDocsFiltered = this.docs.others;
+            }
+        });
+    }
+
+    onClickdownload(url: string, docName: string) {
+        if (!url) {
+            return;
+        }
+        this.helpersService.getPermissionAndDownloadPdf(url, docName);
+    }
+
+    filterDocs(event) {
+		const searchValue = event.srcElement.value;
+
+        if (!searchValue) {
+            return this.otherDocsFiltered = this.docs.others;
+		}
+
+        this.otherDocsFiltered = this.docs.others.filter((doc) => {
+            if (doc.title && searchValue) {
+				if (this.currentLang === 'en') {
+					return (
+						doc.title
+							.toLowerCase()
+							.indexOf(searchValue.toLowerCase()) > -1
+					);
+				} else {
+					return (
+						doc.titleInKhmer
+							.toLowerCase()
+							.indexOf(searchValue.toLowerCase()) > -1
+					);
+				}
             }
 		});
-	}
-
-	onClickdownload (url: string, docName: string) {
-		if (!url) {
-			return;
-		}
-		this.helpersService.getPermissionAndDownloadPdf(url, docName);
-	}
+    }
 
     backHome() {
         this.navCtr.navigateBack("home");
